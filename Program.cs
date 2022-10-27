9f16030a64a8c0b997927940bed73b57e59eadaa
«Vadim <Hazz74@yandex.ru>
2022-10-27 22:57:39 +0500

    creat new console

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите натуральное число от 1 до 7");
+int x=Convert.ToInt32 (Console.ReadLine());
+if (x==1) Console.WriteLine("Mondey");
+if (x==2) Console.WriteLine("Tuesday");
+if (x==3) Console.WriteLine("Wednesday");
+if (x==4) Console.WriteLine("Thursday");
+if (x==5) Console.WriteLine("Friday");
+if (x==6) Console.WriteLine("Saturday");
+if (x==7) Console.WriteLine("Sunday");
