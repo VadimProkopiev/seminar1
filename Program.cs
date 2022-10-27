3686ead80745111241fe5b04c0f63f46d9a59cac
«Vadim <Hazz74@yandex.ru>
2022-10-27 23:06:17 +0500

    упростили код

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿Console.WriteLine("Введите натуральное число от 1 до 7");
 int x=Convert.ToInt32 (Console.ReadLine());
 if (x==1) Console.WriteLine("Mondey");
-if (x==2) Console.WriteLine("Tuesday");
-if (x==3) Console.WriteLine("Wednesday");
-if (x==4) Console.WriteLine("Thursday");
-if (x==5) Console.WriteLine("Friday");
-if (x==6) Console.WriteLine("Saturday");
-if (x==7) Console.WriteLine("Sunday");
+else if (x==2) Console.WriteLine("Tuesday");
+else if (x==3) Console.WriteLine("Wednesday");
+else if (x==4) Console.WriteLine("Thursday");
+else if (x==5) Console.WriteLine("Friday");
+else if (x==6) Console.WriteLine("Saturday");
+else if (x==7) Console.WriteLine("Sunday");
+else Console.WriteLine("Вы ввели число некоретный день недели");
