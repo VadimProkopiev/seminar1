af4571f9d3b2b8972563ea7c4d817c3b487d8995
«Vadim <Hazz74@yandex.ru>
2022-10-27 23:17:02 +0500

    Сделали защиту от ошибок

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите натуральное число от 1 до 7");
+﻿try
+{
+   Console.WriteLine("Введите натуральное число от 1 до 7");
 int x=Convert.ToInt32 (Console.ReadLine());
 if (x==1) Console.WriteLine("Mondey");
 else if (x==2) Console.WriteLine("Tuesday");
@@ -7,4 +9,9 @@ else if (x==4) Console.WriteLine("Thursday");
 else if (x==5) Console.WriteLine("Friday");
 else if (x==6) Console.WriteLine("Saturday");
 else if (x==7) Console.WriteLine("Sunday");
-else Console.WriteLine("Вы ввели число некоретный день недели");
+else Console.WriteLine("Вы ввели число некорhетный день недели"); 
+}
+catch
+{
+    Console.WriteLine("Вы ввели число некорhетный данные"); 
+}
